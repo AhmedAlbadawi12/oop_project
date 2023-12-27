62a71746ab6b5072a248957e04ae03be8125d188
AhmedAlbadawi <medogaming5757@gmail.com>
2023-12-27 21:03:18 +0200

    edited InOut class

--- src/utils/InOut.java
+++ src/utils/InOut.java
@@ -6,4 +6,25 @@ public class InOut {
     public static void printInformation(String message) {
         JOptionPane.showMessageDialog(null, message, "Information", JOptionPane.INFORMATION_MESSAGE);
     }
+
+    public static void printWarning(String message) {
+        JOptionPane.showMessageDialog(null, message, "Warning", JOptionPane.WARNING_MESSAGE);
+    }
+
+    public static void printError(String message) {
+        JOptionPane.showMessageDialog(null, message, "Error", JOptionPane.ERROR_MESSAGE);
+    }
+
+    public static int inputInt(String message) {
+        return Integer.parseInt(JOptionPane.showInputDialog(message));
+
+    }
+
+    public static double inputDouble(String message) {
+        return Double.parseDouble(JOptionPane.showInputDialog(message));
+    }
+
+    public static String inputString(String message) {
+        return JOptionPane.showInputDialog(message);
+    }
 }
