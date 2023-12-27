f36669fe2d4aeb68b0e456999dd2ae10500e08b9
AdhamElshabasy <Adhamnasser@Live.com>
2023-12-27 20:45:22 +0200

    edited InOut Class

--- src/utils/InOut.java
+++ src/utils/InOut.java
@@ -1,4 +1,9 @@
 package utils;
 
+import javax.swing.JOptionPane;
+
 public class InOut {
+    public static void printInformation(String message) {
+        JOptionPane.showMessageDialog(null, message, "Information", JOptionPane.INFORMATION_MESSAGE);
+    }
 }
