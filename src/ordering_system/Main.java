50a82a34644f629bc098e155584422bad48a6480
AdhamElshabasy <Adhamnasser@Live.com>
2024-01-03 20:31:35 +0200

    Added parts of Main

--- src/ordering_system/Main.java
+++ src/ordering_system/Main.java
@@ -6,25 +6,64 @@ public class Main {
     public static void main(String[] args) {
         ShoppingCart cart = new ShoppingCart();
 
-        Item i1 = new Item("Pizza", 2, 2.0);
-        Item i2 = new Item("Cheeseburger", 2, 2.0);
+        String[] productNames = {"Pizza", "Cheeseburger", "Coffee", "Soda", "Water", "Cake"};
+        double[] productPrices = {40.0, 20.0, 50., 4.0, 2.0, 50.0};
 
-        cart.add(i1);
-        cart.add(i1);
-        cart.add(i2);
-        cart.add(i1);
+        while (true) {
+            String mainMenuString = """
+                    Please make a choice
+                                        
+                    1. Add item
+                    2. Remove item
+                    3. Checkout
+                    4. Exit
+                                        
+                    Enter Choice:
+                    """;
 
-        int removeIndex = InOut.inputInt(cart.printCart() + "\n" + "Choose item to remove:");
+            int menuChoice = InOut.inputInt(mainMenuString);
 
-        cart.remove(cart.orderItems[removeIndex - 1]);
+            switch (menuChoice) {
+                case 1:
+                    // Create a new string builder to create multi-line string
+                    StringBuilder foodMenu = new StringBuilder();
 
-        InOut.printInformation(cart.printCart());
+                    // Append the first line
+                    foodMenu.append("Please, select a product:\n\n");
 
-        PayPal p1 = new PayPal("Adham@gmail.com", "Password132");
+                    // Append the menu of items
+                    for (int i = 0; i < productNames.length; i++) {
+                        foodMenu.append((i + 1)).append(". ").append(productNames[i]).append(" - $").append(productPrices[i]).append("\n");
+                    }
 
-        InOut.printInformation(cart.checkout(p1));
+                    // Append the choice line
+                    foodMenu.append("\nEnter choice:");
 
-//        InOut.printInformation(""+cart.getTotalAmount());
+                    String foodMenuString = foodMenu.toString();
 
+                    int foodChoice = InOut.inputInt(foodMenuString);
+                    int quantity = InOut.inputInt("Enter Quantity:");
+                    Item item = new Item(productNames[foodChoice - 1], quantity, productPrices[foodChoice - 1]);
+
+                    cart.add(item);
+
+                    break;
+
+                case 2:
+                    int removeChoice = InOut.inputInt(cart.printCart() + "\n" + "Select Item to remove:");
+                    cart.remove(cart.orderItems[removeChoice - 1]);
+
+                    break;
+
+                case 3:
+                    break;
+
+                case 4:
+                    return;
+
+                default:
+                    InOut.printWarning("Invalid Choice, Try again.");
+            }
+        }
     }
 }
